e9409f3b89d5b86ac7558ca80b8279d0afa022b3
tylerbarton <tylervbarton@gmail.com>
2022-06-13 17:04:20 -0500

    Fix BeerController test methods broken by NonNull validation checks

--- src/test/java/com/tylerbarton/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/tylerbarton/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package com.tylerbarton.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tylerbarton.msscbeerservice.web.model.BeerDto;
+import com.tylerbarton.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,6 +25,19 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    /**
+     * Creates a beerDto with NonNull parameters supplied to pass validation checks.
+     * @return validation-approved dto
+     */
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder()
+                .beerName("testbeer")
+                .beerStyle(BeerStyleEnum.IPA)
+                .price(new BigDecimal("5.95"))
+                .upc(123891231L)
+                .build();
+    }
+
     @Test
     void getBeerById() throws Exception {
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
@@ -37,7 +52,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
         // Initialize the test object
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -48,7 +63,7 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
