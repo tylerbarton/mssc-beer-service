b2b675fbee8a201e597081e7360c8fe82cecbd04
tylerbarton <tylervbarton@gmail.com>
2022-06-13 12:54:30 -0500

    Add BeerLoader to initialize beer objects. Closes #6

---
+++ src/main/java/com/tylerbarton/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,65 @@
+package com.tylerbarton.msscbeerservice.bootstrap;
+
+import com.tylerbarton.msscbeerservice.domain.Beer;
+import com.tylerbarton.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * Creates beer objects via bootstrapping
+ * Implements CommandLineRunner to call on start up.
+ */
+@Component // Required so Spring context picks it up
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    /**
+     * Initialize beerloader with a repository
+     * @param beerRepository Repository responsible for holding beer data
+     */
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    /**
+     * Runs the bean within a SpringApplication
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    /**
+     * Loads the beer objects from the repository
+     */
+    private void loadBeerObjects(){
+        if(beerRepository.count() == 0){
+            // If no beers contained, create
+            beerRepository.save(Beer.builder()
+                            .beerName("original beer")
+                            .beerStyle("IPA")
+                            .quantityToBrew(100)
+                            .minOnHand(12)
+                            .upc(3370102391203L)
+                            .price(new BigDecimal("5.95"))
+                        .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("new aged beer")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(3370102391204L)
+                    .price(new BigDecimal("6.95"))
+                    .build());
+        }
+
+        // Log the loaded beers
+        System.out.println("Loaded Beers:" + beerRepository.count());
+    }
+
+}
--- src/main/java/com/tylerbarton/msscbeerservice/domain/Beer.java
+++ src/main/java/com/tylerbarton/msscbeerservice/domain/Beer.java
@@ -24,7 +24,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator="UUID")
     @GenericGenerator(name="UUID", strategy="org.hibernate.id.UUIDGenerator")
-    @Column(length=36, columnDefinition="varchar", updatable=false, nullable=false)
+    @Column(length=36, columnDefinition="varchar(36)", updatable=false, nullable=false)
     private UUID io;
 
     // Version Properties
