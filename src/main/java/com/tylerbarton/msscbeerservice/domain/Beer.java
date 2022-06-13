295e2f5b429f0a87af5f4cdf8a5f7587da371b24
tylerbarton <tylervbarton@gmail.com>
2022-06-13 12:01:04 -0500

    Add JPA entity for beer. Closes #4

---
+++ src/main/java/com/tylerbarton/msscbeerservice/domain/Beer.java
@@ -0,0 +1,49 @@
+package com.tylerbarton.msscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * Creates a JPA entity for beer so that it may be stored in a database
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+    // Identifying information
+    @Id
+    @GeneratedValue(generator="UUID")
+    @GenericGenerator(name="UUID", strategy="org.hibernate.id.UUIDGenerator")
+    @Column(length=36, columnDefinition="varchar", updatable=false, nullable=false)
+    private UUID io;
+
+    // Version Properties
+    @Version
+    private Long version;
+
+    // Descriptive data
+    @CreationTimestamp
+    @Column(updatable=false)                // Creation is static
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;               // could be BeerStyleEnum later
+    @Column(unique=true)                    // upc should be unique
+    private Long upc;
+    private BigDecimal price;               // money represented by BigDecimal
+
+    // Quantity data
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
