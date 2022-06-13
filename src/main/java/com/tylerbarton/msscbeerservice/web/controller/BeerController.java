3548935dcfb58a8e54baacb9d5c55fe5e52135eb
tylerbarton <tylervbarton@gmail.com>
2022-06-12 19:02:31 -0500

    Add beer controller. Closes #2

--- src/main/java/com/tylerbarton/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/tylerbarton/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,39 @@
 package com.tylerbarton.msscbeerservice.web.controller;
 
+import com.tylerbarton.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
 
+    // TODO: create beer service
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        // TODO: impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        // TODO: impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        // TODO: impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeerById(@PathVariable("beerId") UUID beerId){
+        // TODO: impl
+    }
 }
